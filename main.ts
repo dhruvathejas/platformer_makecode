f5db1f434006ec8898c503aab4f136bd620839d8
dhruvathejas <dhruvathejasb@gmail.com>
2024-11-20 20:47:00 +0530

    Music Gilroy

--- main.ts
+++ main.ts
@@ -14,5 +14,5 @@ game.onUpdate(function () {
     }
 })
 forever(function () {
-    music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("A F E F D G E F ", 120), music.PlaybackMode.UntilDone)
 })
